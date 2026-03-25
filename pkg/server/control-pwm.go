e0b1bc1bcd8ac7c7f9d6d7f6a5f488556d8900f1
darckfast <21337578+Darckfast@users.noreply.github.com>
2026-03-25 18:55:35 -0300

    fix: fixed init order

--- pkg/server/control-pwm.go
+++ pkg/server/control-pwm.go
@@ -1,27 +1,37 @@
 package server
 
 import (
+	"log"
 	"os"
+	"rc/shared"
 )
 
-const servo_pwm = "/sys/devices/platform/fe6f0010.pwm/pwm/pwmchip1/" // PIN_18
-// const esc_pwm = "/sys/devices/platform/fe6f0000.pwm/pwm/pwmchip0/"   // PIN_16
+const servo_pwm_pin_18 = "/sys/devices/platform/fe6f0010.pwm/pwm/pwmchip1/" // PIN_18
+const esc_pwm_pin_16 = "/sys/devices/platform/fe6f0000.pwm/pwm/pwmchip0/"   // PIN_16
 
-const servo_neutral_duty_cycle = "1500000"
-const servo_polarity = "normal"
-const servo_period = "20000000"
+const neutral_duty_cycle = "1500000"
+const polarity = "normal"
+const period = "20000000"
 
 func InitPins() {
-	_, err := os.Stat(servo_pwm)
+	setInitParams(servo_pwm_pin_18, period, neutral_duty_cycle, polarity)
+	log.Println("servo pwm enabled")
+
+	setInitParams(esc_pwm_pin_16, period, neutral_duty_cycle, polarity)
+	log.Println("esc pwm enabled")
+}
+
+func setInitParams(path, period, neutral_duty_cycle, polarity string) {
+	_, err := os.Stat(path)
 
 	if err != nil {
 		panic(err)
 	}
 
-	_, err = os.Stat(servo_pwm + "pwm0")
+	_, err = os.Stat(path + "pwm0")
 
 	if err != nil && os.IsNotExist(err) {
-		err = os.WriteFile(servo_pwm+"export", []byte("0"), 0200)
+		err = os.WriteFile(path+"export", []byte("0"), 0200)
 
 		if err != nil {
 			panic(err)
@@ -30,27 +40,31 @@ func InitPins() {
 		panic(err)
 	}
 
-	err = os.WriteFile(servo_pwm+"pwm0/polarity", []byte(servo_polarity), 0644)
+	err = os.WriteFile(path+"pwm0/period", []byte(period), 0644)
 
 	if err != nil {
 		panic(err)
 	}
 
-	err = os.WriteFile(servo_pwm+"pwm0/period", []byte(servo_period), 0644)
+	err = os.WriteFile(path+"pwm0/duty_cycle", []byte(neutral_duty_cycle), 0644)
 
 	if err != nil {
 		panic(err)
 	}
 
-	err = os.WriteFile(servo_pwm+"pwm0/duty_cycle", []byte(servo_neutral_duty_cycle), 0644)
+	err = os.WriteFile(path+"pwm0/polarity", []byte(polarity), 0644)
 
 	if err != nil {
 		panic(err)
 	}
 
-	err = os.WriteFile(servo_pwm+"pwm0/enable", []byte("1"), 0644)
+	err = os.WriteFile(path+"pwm0/enable", []byte("1"), 0644)
 
 	if err != nil {
 		panic(err)
 	}
+}
+
+func Move(gamepad *shared.NormalizedGamepad) {
+	log.Println(gamepad.Lx, gamepad.Ly)
 }
